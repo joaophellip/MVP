d48ee1a8c3ffe41e200bccd5deb7e0f9b19bb924
Joao Mello <joaophellip@gmail.com>
2018-08-01 14:45:19 -0300

    Added Missing Files

---
+++ app/src/main/java/com/cozo/cozomvp/mainActivity/DetailsInterface.kt
@@ -0,0 +1,13 @@
+package com.cozo.cozomvp.mainActivity
+
+interface DetailsInterface {
+
+    interface MainActivityListener {
+
+        /*
+        Informs activity that order button was clicked by user.
+         */
+        fun onOrderButtonClicked()
+    }
+
+}
