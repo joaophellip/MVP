1179c8e23918c66382818f8786c1ba6ec7ab8054
Joao Mello <joaophellip@gmail.com>
2018-09-25 20:38:13 -0300

    removed anonymous object instances when calling dataProvider methods

--- app/src/main/java/com/cozo/cozomvp/listfragment/ListPresenter.kt
+++ app/src/main/java/com/cozo/cozomvp/listfragment/ListPresenter.kt
@@ -21,23 +21,33 @@ class ListPresenter : MvpBasePresenter<ListFragmentView>(), ListInterfaces.Prese
     }
 
     override fun getActivity(): MainActivity? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var mActivity : MainActivity? = null
+        ifViewAttached {
+            mActivity = it.onActivityRequired()
+        }
+        return mActivity
     }
 
     override fun onRestCardDataRequestCompleted(cards: MutableMap<String, CardMenuData>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        ifViewAttached {
+            restListSize = cards.size
+            it.addRestaurantsDataToCards(cards)
+        }
     }
 
     override fun onRestCardDataRequestFailed(e: Throwable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // do something later
     }
 
     override fun onPartCardDataRequestCompleted(cards: MutableMap<String, CardInfoData>) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        ifViewAttached {
+            partListSize = cards.size
+            it.addPartnersDataToCards(cards)
+        }
     }
 
     override fun onPartCardDataRequestFailed(e: Throwable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // do something later
     }
 
     override fun onRestItemsDataRequestCompleted(items: List<NetworkModel.MenuMetadata>) {
@@ -47,7 +57,7 @@ class ListPresenter : MvpBasePresenter<ListFragmentView>(), ListInterfaces.Prese
     }
 
     override fun onRestItemsDataRequestFailed(e: Throwable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // do something later
     }
 
     override  fun onUserLocationDataAvailable(location: NetworkModel.Location){
@@ -61,70 +71,13 @@ class ListPresenter : MvpBasePresenter<ListFragmentView>(), ListInterfaces.Prese
 
     // retrieves data for nearby restaurants from Data Provider and sends them to the list view.
     private fun getNearbyRestaurantsCardData(location: NetworkModel.Location){
-        // refactor DataProvider object to be constant instead of anonymous!!!!!!!!!!!!!!!! By Lucas
-        // Divida Técnica ??? Parece que sim
-        mDataProvider = DataProvider(object : DataProviderInterface.ListFragmentListener {
-            override fun onRestItemsDataRequestCompleted(items: List<NetworkModel.MenuMetadata>) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onRestItemsDataRequestFailed(e: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onPartCardDataRequestCompleted(cards: MutableMap<String,CardInfoData>) {}
-            override fun onPartCardDataRequestFailed(e: Throwable) {}
-            override fun onRestCardDataRequestCompleted(cards: MutableMap<String,CardMenuData>) {
-                ifViewAttached {
-                    restListSize = cards.size
-                    it.addRestaurantsDataToCards(cards)
-                }
-            }
-            override fun onRestCardDataRequestFailed(e: Throwable) {
-                Log.d("MVPdebug", "error na chamada http do menu - expected")
-                //do something later
-            }
-            override fun getActivity(): MainActivity? {
-                var mActivity : MainActivity? = null
-                ifViewAttached {
-                    mActivity = it.onActivityRequired()
-                }
-                return mActivity
-            }
-        })
+        mDataProvider = DataProvider(this)
         mDataProvider?.provideRestaurantsCardData(location,10000)
     }
 
     // retrieves list of nearby delivery partners from Data Provider and sends them to the list view.
     private fun getNearbyDeliveryPartnersCardData(restLocation: NetworkModel.Location, userLocation: NetworkModel.Location){
-        mDataProvider = DataProvider(object : DataProviderInterface.ListFragmentListener {
-            override fun onRestItemsDataRequestCompleted(items: List<NetworkModel.MenuMetadata>) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onRestItemsDataRequestFailed(e: Throwable) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onPartCardDataRequestCompleted(cards: MutableMap<String, CardInfoData>) {
-                ifViewAttached {
-                    partListSize = cards.size
-                    it.addPartnersDataToCards(cards)
-                }
-            }
-            override fun onPartCardDataRequestFailed(e: Throwable) {
-                //do something later
-            }
-            override fun onRestCardDataRequestCompleted(cards: MutableMap<String,CardMenuData>) {}
-            override fun onRestCardDataRequestFailed(e: Throwable) {}
-            override fun getActivity(): MainActivity? {
-                var mActivity : MainActivity? = null
-                ifViewAttached {
-                    mActivity = it.onActivityRequired()
-                }
-                return mActivity
-            }
-        })
+        mDataProvider = DataProvider(this)
         mDataProvider?.provideDeliveryPartnersList(restLocation, userLocation)
     }
 
